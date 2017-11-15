57778e4f1e286a08ea6177af369916b48855cd53
borkmaster <unluckymattress@gmail.com>
2017-11-15 13:56:38 -0600

    Added scene load functionality to start menu

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,10 +5,12 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
 		Debug.Log ("Load level requested for " + name);
+		Application.LoadLevel (name);
 	}
 
 	public void QuitRequest () {
 		Debug.Log ("Quit requested");
+		Application.Quit ();
 	}
 
 }
