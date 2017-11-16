c544dd460a2ab73a9875d392ed45fe431d1b7c52
unluckymattress <unluckymattress@gmail.com>
2017-11-15 21:00:18 -0600

    All game scenes now connected
    All scenes are reachable from the Start scene and all scene paths
    eventually loop back to the Start scene

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,7 +5,7 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
 		Debug.Log ("Load level requested for " + name);
-		Application.LoadLevel (name);
+		Application.LoadLevel(name);
 	}
 
 	public void QuitRequest () {
