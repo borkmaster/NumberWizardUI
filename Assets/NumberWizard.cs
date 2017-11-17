1caf7e36db7d92ce4b58988e18256573f0174b65
unluckymattress <unluckymattress@gmail.com>
2017-11-16 20:37:25 -0600

    Added UI elements to allow player to interact with game

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	int maxGuessesAllowed = 10;
+
+	public Text guessText;
+
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+
+		max += 1;
+		guessText.text = "Is your number: " + guess.ToString() + "?";
+	}
+
+	void NextGuess () {
+		guess = (max + min) / 2;
+		guessText.text = "Is your number: " + guess.ToString() + "?";
+		maxGuessesAllowed -= 1;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess ();
+	}
+
+}
